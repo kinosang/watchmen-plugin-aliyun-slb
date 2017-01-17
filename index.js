df03d38e2d36e920a3fe48839b3762fda67da97d
7IN0SAN9 <me@7in0.me>
2017-01-17 23:12:29 +0800

    Init.

--- index.js
+++ index.js
@@ -1,86 +1,135 @@
-var _ = require('lodash');
-var moment = require('moment');
-var validator = require('validator');
-var debug = require('debug')('notifications');
-var awsFactory = require('./lib/aws-ses.js');
+var ALY = require('aliyun-sdk');
 
-var EMAIL_SUBJECT_PREFIX = '[watchmen]';
-
-var config = {
-  from: process.env.WATCHMEN_AWS_FROM,
-  region: process.env.WATCHMEN_AWS_REGION,
-  AWS_KEY: process.env.WATCHMEN_AWS_KEY,
-  AWS_SECRET: process.env.WATCHMEN_AWS_SECRET
-};
-
-function sendNotificationIfPeopleSubscribed(service, options){
-  var rcpts = getRecipients(service);
-  if (rcpts.length) {
-    var aws = new awsFactory(config);
-    var options = {
-      to: rcpts,
-      title: options.title,
-      body: options.body
-    };
-    aws.send(options, function (err, data) {
-      if (err) {
-        console.log('error sending notification for ' + service.name);
-        console.error(err);
-      } else {
-        console.log('notification sent successfully to ' + rcpts + ' for ' + service.name);
-      }
-    });
-  }
-}
+var slb = new ALY.SLB({
+    accessKeyId: process.env.WATCHMEN_ALIYUN_ACCESS_ID,
+    secretAccessKey: process.env.WATCHMEN_ALIYUN_ACCESS_KEY,
+    endpoint: 'https://slb.aliyuncs.com',
+    apiVersion: '2014-05-15'
+});
 
 var eventHandlers = {
 
-  /**
-   * On a new outage
-   * @param service
-   * @param outage
-   */
+    /**
+     * On a new outage
+     * @param {Object} service
+     * @param {Object} outage
+     * @param {Object} outage.error check error
+     * @param {number} outage.timestamp outage timestamp
+     */
 
-  onNewOutage: function (service, outage) {
-    debug('triggering "onNewOutage" notification');
-    sendNotificationIfPeopleSubscribed(service, {
-      title: EMAIL_SUBJECT_PREFIX + ' ' + service.name + ' is down!',
-      body: service.name + ' is down!. Reason: ' + JSON.stringify(outage.error)
-    })
-  },
+    onNewOutage: function(service, outage) {
+        var region = ["cn-beijing", "cn-qingdao", "cn-hangzhou", "cn-hongkong", "cn-shenzhen", "us-west-1"];
+        var tag = service.name.split(" ");
 
-  /**
-   * Service is back up online
-   * @param service
-   * @param lastOutage
-   */
+        var isRegion = region.filter(function(item) {
+            return item == tag[0]
+        }).length ? true : false;
 
-  onServiceBack: function (service, lastOutage) {
-    debug('triggering "onServiceBack" notification');
-    sendNotificationIfPeopleSubscribed(service, {
-      title: EMAIL_SUBJECT_PREFIX + ' ' + service.name + ' is back!',
-      body: service.name + ' down for ' + moment.duration(lastOutage.downtime).humanize() +
-      '. Error: ' + JSON.stringify(lastOutage.error)
-    });
-  }
+        if (isRegion) {
+            slb.describeLoadBalancers({
+                RegionId: tag[0],
+                ServerId: tag[1]
+            }, function(err, res) {
+                var LoadBalancer = res.LoadBalancers.LoadBalancer[0];
 
+                slb.describeLoadBalancerAttribute({
+                    LoadBalancerId: LoadBalancer.LoadBalancerId
+                }, function(err, oldLoadBalancer) {
+                    slb.deleteLoadBalancer({
+                        LoadBalancerId: oldLoadBalancer.LoadBalancerId
+                    }, function(err) {
+                        if (!err) {
+                            slb.createLoadBalancer({
+                                RegionId: oldLoadBalancer.RegionId,
+                            }, , function(err, newLoadBalancer) {
+                                oldLoadBalancer.ListenerPorts.ListenerPort.forEach(function(port) {
+                                    slb.createLoadBalancerTCPListener({
+                                        LoadBalancerId: newLoadBalancer.LoadBalancerId,
+                                        ListenerPort: port,
+                                        BackendServerPort: port,
+                                        Bandwidth: -1,
+                                    });
+                                });
+
+                                slb.addBackendServers({
+                                    LoadBalancerId: newLoadBalancer.LoadBalancerId,
+                                    BackendServers: oldLoadBalancer.BackendServers.BackendServer,
+                                });
+                            });
+                        }
+                    });
+                });
+            });
+        }
+    },
+
+    /**
+     * Failed ping on an existing outage
+     * @param {Object} service
+     * @param {Object} outage
+     * @param {Object} outage.error check error
+     * @param {number} outage.timestamp outage timestamp
+     */
+
+    onCurrentOutage: function(service, outage) {
+        //
+    },
+
+    /**
+     * Failed check (it will be an outage or not according to service.failuresToBeOutage
+     * @param {Object} service
+     * @param {Object} data
+     * @param {Object} data.error check error
+     * @param {number} data.currentFailureCount number of consecutive check failures
+     */
+
+    onFailedCheck: function(service, data) {
+        //
+    },
+
+    /**
+     * Warning alert
+     * @param {Object} service
+     * @param {Object} data
+     * @param {number} data.elapsedTime (ms)
+     */
+
+    onLatencyWarning: function(service, data) {
+        //
+    },
+
+    /**
+     * Service is back online
+     * @param {Object} service
+     * @param {Object} lastOutage
+     * @param {Object} lastOutage.error
+     * @param {number} lastOutage.timestamp (ms)
+     */
+
+    onServiceBack: function(service, lastOutage) {
+        //
+    },
+
+    /**
+     * Service is responding correctly
+     * @param {Object} service
+     * @param {Object} data
+     * @param {number} data.elapsedTime (ms)
+     */
+
+    onServiceOk: function(service, data) {
+        //
+    }
 };
 
-function getRecipients(service){
-  function parseEmailStringsToArray (commaSeparatedEmails){
-    var emails = (commaSeparatedEmails || "").split(',');
-    return emails.map(function(s){ return s.trim()}).filter(function(i){return validator.isEmail(i)});
-  }
-  return _.union(
-      parseEmailStringsToArray(service.alertTo),
-      parseEmailStringsToArray(process.env.WATCHMEN_NOTIFICATIONS_ALWAYS_ALERT_TO)
-  );
+function ConsolePlugin(watchmen) {
+    watchmen.on('new-outage', eventHandlers.onNewOutage);
+    //watchmen.on('current-outage', eventHandlers.onCurrentOutage);
+    //watchmen.on('service-error', eventHandlers.onFailedCheck);
+
+    //watchmen.on('latency-warning', eventHandlers.onLatencyWarning);
+    //watchmen.on('service-back', eventHandlers.onServiceBack);
+    //watchmen.on('service-ok', eventHandlers.onServiceOk);
 }
 
-function AwsSesPlugin (watchmen){
-  watchmen.on('new-outage', eventHandlers.onNewOutage);
-  watchmen.on('service-back', eventHandlers.onServiceBack);
-}
-
-exports = module.exports = AwsSesPlugin;
-
+exports = module.exports = ConsolePlugin;
